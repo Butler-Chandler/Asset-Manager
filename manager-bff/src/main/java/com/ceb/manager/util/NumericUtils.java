f87ea29eab0487844a7861a8fdbe964bfc608c03
Chandler Butler <Chandlerbutler0@gmail.com>
2020-05-08 15:37:42 -0400

    Adding numeric utils that didnt get staged

---
+++ manager-bff/src/main/java/com/ceb/manager/util/NumericUtils.java
@@ -0,0 +1,12 @@
+package com.ceb.manager.util;
+
+public class NumericUtils {
+
+    public static boolean hasLong(Long num){
+        if(num != null){
+            return true;
+        }
+        return false;
+    }
+    
+}
