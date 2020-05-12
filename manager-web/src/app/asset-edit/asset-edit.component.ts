d1a25739f244ddd4bf1ce20336706a67053454ea
Chandler Butler <Chandlerbutler0@gmail.com>
2020-05-12 16:04:14 -0400

    Minor bug fixing in java tier and angular setup

---
+++ manager-web/src/app/asset-edit/asset-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-edit',
+  templateUrl: './asset-edit.component.html',
+  styleUrls: ['./asset-edit.component.css']
+})
+export class AssetEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ manager-web/src/app/asset-get/asset-get.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-get',
+  templateUrl: './asset-get.component.html',
+  styleUrls: ['./asset-get.component.css']
+})
+export class AssetGetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
